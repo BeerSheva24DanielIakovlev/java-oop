646b8a474136b8e9775501354ec802f30206bbc1
DaniilJustCode <gromdan90@gmail.com>
2024-08-04 13:07:24 +0300

    fixed method addShape (method call)

--- src/main/java/telran/shapes/Canvas.java
+++ src/main/java/telran/shapes/Canvas.java
@@ -8,17 +8,18 @@ public class Canvas implements Shape{
         shapes = new Shape[0];
     }
     public void addShape(Shape shape) {
-        Shape[] newShapes = new Shape[shapes.length + 1];
+        /*Shape[] newShapes = new Shape[shapes.length + 1];
         System.arraycopy(shapes, 0, newShapes, 0, shapes.length);
         newShapes[shapes.length]=shape;
-        shapes = newShapes;
+        shapes = newShapes;*/
+        shapes = Arrays.insert(shapes, shapes.length, shape);
     }
         @Override
         public int perimiter() {
             int newPerimiter = 0;
 
             for(Shape j : shapes) {
-                newPerimiter = newPerimiter + j.perimiter();
+                newPerimiter += + j.perimiter();
             }
             return newPerimiter;
             // TODO 
@@ -29,7 +30,7 @@ public class Canvas implements Shape{
         public int area() {
             int newArea = 0;
             for(Shape j : shapes) {
-                newArea = newArea + j.area();
+                newArea += j.area();
             }
             return newArea;
             // TODO 
